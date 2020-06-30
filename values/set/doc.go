ce018fec04d9bbb84db5401158116136adff1b98
Nupur Agrawal <nupur202000@gmail.com>
2020-06-30 19:57:11 +0530

    adds set package

---
+++ values/set/doc.go
@@ -0,0 +1,6 @@
+// Copyright (c) 2020 SDSLabs
+// Use of this source code is governed by an MIT license
+// details of which can be found in the LICENSE file.
+
+// Package set implements a kiwi.Value which can store a set of strings.
+package set
---
+++ values/set/value.go
@@ -0,0 +1,169 @@
+// Copyright (c) 2020 SDSLabs
+// Use of this source code is governed by an MIT license
+// details of which can be found in the LICENSE file.
+
+package set
+
+import (
+	"fmt"
+
+	"github.com/sdslabs/kiwi"
+)
+
+func init() {
+	kiwi.RegisterValue(func() kiwi.Value { return new(Value) })
+}
+
+// Type of set value.
+const Type kiwi.ValueType = "set"
+
+// Value can store a set of strings.
+//
+// It implements the kiwi.Value interface.
+type Value map[string]struct{}
+
+// Various errors for set value type.
+var (
+	ErrInvalidParamLen  = fmt.Errorf("not enough parameters")
+	ErrInvalidParamType = fmt.Errorf("invalid paramater type")
+)
+
+// newParamLenErr creates an error where parameter length is wrong.
+func newParamLenErr(u, l int) error {
+	return fmt.Errorf("%w: got %d; requires %d", ErrInvalidParamLen, u, l)
+}
+
+// newParamTypeErr creates an error where parameter type is wrong.
+func newParamTypeErr(p, e interface{}) error {
+	typ := fmt.Sprintf("%T", e)
+	return fmt.Errorf("%w: %#v not a(n) %q", ErrInvalidParamType, p, typ)
+}
+
+const (
+	// Insert inserts the string in the set.
+	//
+	// Returns the added string.
+	Insert kiwi.Action = "INSERT"
+
+	// Remove removes the string from the set.
+	// If no index is provided it gets the last element.
+	//
+	// Returns removed string.
+	Remove kiwi.Action = "REMOVE"
+
+	// Has checks if set has the element.
+	//
+	// Returns boolean value.
+	Has kiwi.Action = "HAS"
+
+	// Len gets the length of the set.
+	//
+	// Returns an integer.
+	Len kiwi.Action = "LEN"
+
+	// Get gets all the elements of set.
+	//
+	// Returns an array of elements.
+	Get kiwi.Action = "GET"
+)
+
+// Type returns v's type, i.e., "set".
+func (v *Value) Type() kiwi.ValueType {
+	return Type
+}
+
+// DoMap returns the map of v's actions with it's do functions.
+func (v *Value) DoMap() map[kiwi.Action]kiwi.DoFunc {
+	return map[kiwi.Action]kiwi.DoFunc{
+		Insert: v.insert,
+		Remove: v.remove,
+		Has:    v.has,
+		Len:    v.len,
+		Get:    v.get,
+	}
+}
+
+// insert implements the INSERT action.
+func (v *Value) insert(params ...interface{}) (interface{}, error) {
+	if len(params) == 0 {
+		return nil, newParamLenErr(0, 1)
+	}
+
+	var (
+		toInsert string
+		ok       bool
+	)
+
+	out := make([]string, len(params))
+	for i := 0; i < len(params); i++ {
+		toInsert, ok = params[i].(string)
+		if !ok {
+			return nil, newParamTypeErr(params[i], toInsert)
+		}
+		(*v)[toInsert] = struct{}{}
+		out[i] = toInsert
+	}
+	return out, nil
+}
+
+// remove implements the REMOVE action.
+func (v *Value) remove(params ...interface{}) (interface{}, error) {
+	if len(params) == 0 {
+		return nil, newParamLenErr(0, 1)
+	}
+
+	var (
+		toRemove string
+		ok       bool
+	)
+
+	out := make([]string, len(params))
+	for i := 0; i < len(params); i++ {
+		toRemove, ok = params[i].(string)
+		if !ok {
+			return nil, newParamTypeErr(params[i], toRemove)
+		}
+		delete((*v), toRemove)
+		out[i] = toRemove
+	}
+	return out, nil
+}
+
+// has implements the HAS action.
+func (v *Value) has(params ...interface{}) (interface{}, error) {
+	if len(params) != 1 {
+		return nil, newParamLenErr(len(params), 1)
+	}
+
+	toCheck, ok := params[0].(string)
+	if !ok {
+		return nil, newParamTypeErr(params[0], toCheck)
+	}
+
+	_, ok = (*v)[toCheck]
+	return ok, nil
+}
+
+// len implements the LEN action.
+func (v *Value) len(params ...interface{}) (interface{}, error) {
+	if len(params) != 0 {
+		return nil, newParamLenErr(len(params), 0)
+	}
+
+	return len(*v), nil
+}
+
+// get implements the GET action.
+func (v *Value) get(params ...interface{}) (interface{}, error) {
+	if len(params) != 0 {
+		return nil, newParamLenErr(len(params), 0)
+	}
+
+	i := 0
+	out := make([]string, len(*v))
+	for elem := range *v {
+		out[i] = elem
+		i++
+	}
+	return out, nil
+}
