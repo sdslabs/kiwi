75c0548599710cb19fb18940af543d74acf60454
Nupur Agrawal <nupur202000@gmail.com>
2020-07-17 23:09:46 +0530

    Adds zset package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sdslabs/kiwi
 
 go 1.14
+
+require github.com/wangjia184/sortedset v0.0.0-20200422044937-080872f546ba
---
+++ values/zset/doc.go
@@ -0,0 +1,7 @@
+// Copyright (c) 2020 SDSLabs
+// Use of this source code is governed by an MIT license
+// details of which can be found in the LICENSE file.
+
+// Package zset implements a kiwi.Value which can store a set
+// with each element having score.
+package zset
